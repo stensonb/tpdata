4cba575e95e76401df9ace21c759939f93562b9d
Ben Woodall <mail@benwoodall.com>
2012-07-17 18:58:22 -0700

    * Added PUT functionality.   1. This works with the body: as a String   2. URL params are set in an Array in query:
    === Things to add
    1. a 'config' to set query: faster
    2. add POST and DELETE

--- lib/theplatform/data.rb
+++ lib/theplatform/data.rb
@@ -2,6 +2,8 @@ module ThePlatform
 
   class Data
     include HTTParty
+    require 'json'
+    debug_output $stderr
 
     def initialize(params={})
       @endpoint = params[:endpoint]
@@ -14,7 +16,7 @@ module ThePlatform
         define_method(data) { self.new(endpoint: SERVICE[data][:endpoint], objects: SERVICE[data][:objects]) }
       end
     end
-
+    # ThePlatform::Data.mds.get('Category','12744085', schema:'1.4.0',form:'json',token:'12uZynnc2zHvVNDokvgG0TC9UBD7EPDm')
     def get(object, id=[],options={})
       self.class.base_uri @endpoint
       self.class.get("/#{object}/#{id}", query: options)
@@ -25,13 +27,28 @@ module ThePlatform
     #   self.class.post("/#{object}",  query: body)
     # end
 
-    # def put #(object, body)
-    #   self.class.base_uri @endpoint
-    #   self.class.put("/Category/12744085?schema=1.4.0&form=cjson&token=RFSe2R2yqoWqzg_xYdgIcYCbEKAfIMBd", {"id" => "http://data.media.theplatform.com/media/data/Category/12744085","description" => "Hello World"})
-    # end
+    def put(object, body, options={})
+      # options = {body: body, query: {schema: '1.4.0', form: 'json', token: 'ZvQnJThMUUp5w1BxkpgJkaAZYCD-8MCN'}}
+      @options = options
+      self.class.base_uri @endpoint
+      set_header(@options)
+      self.class.put("/#{object}", query: options, body: body)
+    end
 
     # def delete
     # end
+
+    private
+
+    def set_header(option)
+      if option[:form] == 'json'
+        self.class.headers 'Content-Type' => 'application/json'
+      elsif option[:form] == 'atom'
+        self.class.headers 'Content-Type' => 'application/atom+xml'
+      elsif option[:form] == 'rss'
+        self.class.headers 'Content-Type' => 'application/rss+xml'
+      end
+    end
 
   end
 
