a634626550aedb018c943bd8041b98d94fd48e8c
Ben Woodall <mail@benwoodall.com>
2012-08-21 12:15:21 -0700

    moved into a separate module to dry up ident and data class configs

---
+++ lib/theplatform/configuration.rb
@@ -0,0 +1,43 @@
+module ThePlatform
+  module Configuration
+
+    extend self
+
+    attr_accessor :schema, :form, :token, :username, :password, :duration, :timeout
+
+    # Allows for a Rails type configuration setup.  Different object make different configurations.
+    #
+    #    ThePlatform::Data.configure do |config|
+    #      config.schema = '1.4.0'
+    #      config.form = 'cjson'
+    #      config.token = 'o4VThP--IcaKwltckmgdw544KD0kKDg'
+    #    end
+    #    =>  {:schema=>"1.4.0", :form=>"json", :token=>"o4VThP--IcaKwltckmgdw544KD0kKDg"}
+    #
+    def configure
+      yield self
+      parameters
+    end
+
+    # List available parameters and values in those params
+    def parameters
+      @values = {}
+      keys.each { |k| @values.merge! k => get_var("@#{k}") }
+      @values
+    end
+
+    # Returns true or false if all parameters are set.  Only valuable if you want all 3 set.
+    def parameters?
+      parameters.values.all?
+    end
+
+
+    private
+
+    # Helper to clean up recursive method in #parameters
+    def get_var(var)
+      self.instance_variable_get(var)
+    end
+
+  end
+end
