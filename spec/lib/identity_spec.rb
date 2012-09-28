31545e21c240e3a9d89a1fb1eb7188e7016dd20c
Ben Woodall <mail@benwoodall.com>
2012-09-28 16:24:55 -0700

    moving away from responses

--- spec/lib/identity_spec.rb
+++ spec/lib/identity_spec.rb
@@ -2,37 +2,50 @@ require 'spec_helper'
 
 describe ThePlatform::Identity do
 
-  it 'should include HTTParty methods' do
-    ThePlatform::Identity.should include HTTParty
-  end
+  describe 'default attributes' do
 
-  before(:each) do
-    @session = ThePlatform::Identity.token(:username => 'ben.woodall@theplatform.com', :password => 'Behilikus420', schema: '1.0', form: 'json')
+    it 'should include HTTParty methods' do
+      ThePlatform::Identity.should include HTTParty
+    end
+
   end
 
   describe "token" do
+
+    stub_request(:get, ThePlatform::Identity.token(schema:'1.0', form:'json', username:'test_user', password:'password')).
+  to_return(:status => 200, :body => "", :headers => {})
+
+    it 'should work' do
+      ThePlatform::Identity.token(schema:'1.0', form:'json', username:'test_user', password:'password')
+    end
+
     it "should return all auth token params" do
-      res = @session
+      res = @token
       res.should be_a Hash
       res.has_key?("signInResponse")
       token = res["signInResponse"]["token"]
       ThePlatform::Identity.invalidate!(token)
     end
+
   end
 
   describe "invalidate!" do
+
     it "should invalidate a token" do
       res = @session
       token = res['signInResponse']['token']
       ThePlatform::Identity.invalidate!(token).has_key?("signOutResponse")
     end
+
   end
 
   describe "count" do
+
     it "should return the number of tokens" do
       res = @session.count
       res.should be_a Fixnum
     end
+
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
 require 'rspec'
+require 'webmock/rspec'
 
 require File.expand_path("../../lib/theplatform", __FILE__)
