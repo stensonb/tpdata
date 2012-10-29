7881b85206ec0f6626115b0464923a9e40f0cbef
Bryan Stenson <bryan.stenson@gmail.com>
2012-10-29 15:17:43 -0700

    add "rake coverage" to generage test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,9 @@ require 'rspec'
 require 'webmock/rspec'
 require 'json'
 
+if ENV["COVERAGE"] == 'true'
+  require 'simplecov'
+  SimpleCov.start
+end
+
 require File.expand_path("../../lib/theplatform", __FILE__)
