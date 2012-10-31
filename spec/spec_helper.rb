9188d8d809feb94c196f69adb705270be3c81f2c
Bryan Stenson <bryan.stenson@gmail.com>
2012-10-31 07:21:29 -0700

    add simplecov-rcov output formatter to support Jenkins/Sonar

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,17 @@ require 'json'
 
 if ENV["COVERAGE"] == 'true'
   require 'simplecov'
+
+  # add legacy rcov output (so Jenkins/Sonor play well)
+  require 'simplecov-rcov'
+  class SimpleCov::Formatter::MergedFormatter
+    def format(result)
+       SimpleCov::Formatter::HTMLFormatter.new.format(result)
+       SimpleCov::Formatter::RcovFormatter.new.format(result)
+    end
+  end
+  SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
+
   SimpleCov.start
 end
 
--- theplatform.gemspec
+++ theplatform.gemspec
@@ -24,4 +24,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'yard'
   s.add_development_dependency 'webmock'
   s.add_development_dependency 'simplecov'
+  s.add_development_dependency 'simplecov-rcov'
+  
 end
