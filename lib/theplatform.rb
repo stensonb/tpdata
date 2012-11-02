8b9fd72869d6d9ab20fe04bbaa04670623fdc6f3
Ben Woodall <mail@benwoodall.com>
2012-11-02 08:34:47 -0700

    moved versioning to a flat file

--- lib/theplatform.rb
+++ lib/theplatform.rb
@@ -3,6 +3,9 @@ require 'httparty'
 # Start of wrapper for tpdata
 module ThePlatform
 
+  # Set Version
+  VERSION = File.read(File.expand_path("../../tpdata_version",__FILE__)).strip
+
   require 'theplatform/configuration'
   require 'theplatform/services'
   require 'theplatform/identity'
